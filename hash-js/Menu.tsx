98ab04650b7ee526499e5c294110d4c91bc1e54f
Cole Margerum <cole.margerum@gmail.com>
2024-04-04 17:45:21 -0700

    Menu and bootstrap (might remove bootstrap in favor of react bootstrap

---
+++ hash-js/Menu.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function Menu() {
+  return (
+    <>
+      <div>Menu</div>
+      <Text>Play</Text>
+      <Text>Tutorial</Text>
+      <Text>Settings</Text>
+    </>
+  );
+}
+
+export default Menu;
