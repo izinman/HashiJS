aac637b00a8e54aa107a91f1e4ae305080f3f615
Cole Margerum <cole.margerum@gmail.com>
2024-04-15 20:39:21 -0700

    Dynamic Grid Points

--- hashi-js/GridPoint.tsx
+++ hashi-js/GridPoint.tsx
@@ -1,17 +1,81 @@
-import { Text, View, StyleProp, StyleSheet, ViewStyle } from "react-native";
+import {
+  Text,
+  View,
+  StyleProp,
+  StyleSheet,
+  ViewStyle,
+  DimensionValue,
+} from "react-native";
 
 interface Props {
   style: StyleProp<ViewStyle>;
   goalNumber?: number;
+  nodeType: string;
 }
 
-function GridPoint({ style, goalNumber }: Props) {
+function GridPoint({ style, goalNumber, nodeType }: Props) {
+  let lineLength: DimensionValue = goalNumber ? "25%" : "50%";
   return (
     <View style={style}>
       {goalNumber ? (
         <View style={styles.node}>
           <Text>{goalNumber}</Text>
         </View>
+      ) : null}
+
+      {/* Change To Enums */}
+      {!["topLeft", "bottomLeft", "firstColumn"].includes(nodeType) ? (
+        // Left Line
+        <View
+          style={{
+            position: "absolute",
+            left: 0,
+            height: 1,
+            width: lineLength,
+            backgroundColor: "black",
+          }}
+        />
+      ) : null}
+
+      {!["topRight", "bottomRight", "lastColumn"].includes(nodeType) ? (
+        // Right Line
+        <View
+          style={{
+            position: "absolute",
+            right: 0,
+            height: 1,
+            width: lineLength,
+            backgroundColor: "black",
+          }}
+        />
+      ) : null}
+
+      {!["topLeft", "topRight", "firstRow"].includes(nodeType) ? (
+        // Up Line
+        <View
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: 0,
+            height: lineLength,
+            width: 1,
+            backgroundColor: "black",
+          }}
+        />
+      ) : null}
+
+      {!["bottomRight", "bottomLeft", "lastRow"].includes(nodeType) ? (
+        // Down Line
+        <View
+          style={{
+            position: "absolute",
+            left: "50%",
+            bottom: 0,
+            height: lineLength,
+            width: 1,
+            backgroundColor: "black",
+          }}
+        />
       ) : null}
     </View>
   );
--- hashi-js/HashiPuzzleRendered.tsx
+++ hashi-js/HashiPuzzleRendered.tsx
@@ -1,6 +1,7 @@
-import { Dimensions, Text, FlatList, View, StyleSheet } from "react-native";
+import { Dimensions, FlatList, View, StyleSheet } from "react-native";
 import { HashiPuzzle } from "./library/HashiPuzzle";
 import GridPoint from "./GridPoint";
+import { PuzzleMenuBar } from "./PuzzleMenuBar";
 
 interface Props {
   puzzle: HashiPuzzle;
@@ -11,13 +12,35 @@ function HashiPuzzleRendered() {
   let puzzleWidth = 9;
   let puzzleHeight = 9;
   let cells = [];
-  for (let i = 0; i < puzzleWidth * puzzleHeight; i++) {
-    cells.push(1);
+
+  for (let i = 0; i < puzzleHeight + 1; i++) {
+    for (let j = 0; j < puzzleWidth + 1; j++) {
+      if (i == 0 && j == 0) {
+        cells.push({ goalNumber: null, nodeType: "topLeft" });
+      } else if (i == 0 && j == puzzleWidth) {
+        cells.push({ goalNumber: null, nodeType: "topRight" });
+      } else if (i == puzzleHeight && j == 0) {
+        cells.push({ goalNumber: null, nodeType: "bottomLeft" });
+      } else if (i == puzzleHeight && j == puzzleWidth) {
+        cells.push({ goalNumber: null, nodeType: "bottomRight" });
+      } else if (i == 0) {
+        cells.push({ goalNumber: null, nodeType: "firstRow" });
+      } else if (j == 0) {
+        cells.push({ goalNumber: null, nodeType: "firstColumn" });
+      } else if (i == puzzleHeight) {
+        cells.push({ goalNumber: null, nodeType: "lastRow" });
+      } else if (j == puzzleWidth) {
+        cells.push({ goalNumber: null, nodeType: "lastColumn" });
+      } else {
+        cells.push({ goalNumber: null, nodeType: null });
+      }
+    }
   }
 
-  let cellMaxWidth = (Dimensions.get("window").width - 200) / puzzleWidth;
-  let cellMaxHeight = (Dimensions.get("window").height - 200) / puzzleHeight;
-  let sideLength = cellMaxHeight < cellMaxWidth ? cellMaxHeight : cellMaxWidth;
+  let cellMaxWidth = (Dimensions.get("window").width - 100) / (puzzleWidth + 1);
+  let cellMaxHeight =
+    (Dimensions.get("window").height - 100) / (puzzleHeight + 1);
+  let sideLength = Math.min(cellMaxHeight, cellMaxWidth);
 
   return (
     <>
@@ -27,17 +50,16 @@ function HashiPuzzleRendered() {
           renderItem={(cell) => (
             <GridPoint
               style={{
-                borderColor: "black",
-                borderWidth: 0.5,
                 width: sideLength,
                 height: sideLength,
                 justifyContent: "center",
                 alignItems: "center",
               }}
-              goalNumber={cell.item}
+              goalNumber={cell.item.goalNumber}
+              nodeType={cell.item.nodeType}
             />
           )}
-          numColumns={puzzleWidth}
+          numColumns={puzzleWidth + 1}
           scrollEnabled={false}
         />
       </View>
@@ -49,8 +71,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    marginHorizontal: 100,
-    marginVertical: 100,
+    marginHorizontal: 50,
+    marginVertical: 50,
   },
 });
 
